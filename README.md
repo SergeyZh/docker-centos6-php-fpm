b7d297adf5845068cafe51a0fbf88985ddf219c1
Sergey Zhukov <sergey@jetbrains.com>
2014-08-11 18:58:35 +0400

    Added PHPINI_xxxx variables to modify php.ini on startup

--- README.md
+++ README.md
@@ -5,3 +5,10 @@ CentOS6 + php-fpm daemon
 
 Use -e PHP_FPM_USER=<user id> to run php-fpm under other Unix account. 
 It may be necessary to access PHP files.
+
+You may set variables like `PHPINI_XXXX=Value` to set variable `XXXX = Value` at `/etc/php.ini` file.
+For example `-e PHPINI_upload_max_filesize=5M` will set upload_max_filesize = 5M at php.ini.
+
+If you need to set variable with dot `.` inside, you may use double underscore `__`.
+For example `-e PHPINI_soap__wsdl_cache_enabled=1` will set soap.wsdl_cache_enabled = 1 at php.ini.
+
--- run-services.sh
+++ run-services.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+modify_php_ini()
+{
+    echo "PHP Param: $1"
+    NAME=`echo $1 | cut -d '=' -f 1`
+    VALUE=`echo $1 | cut -d '=' -f 2`
+    sed -i "/^${NAME}/ s/${NAME}.*/${NAME}\ =\ ${VALUE}/" /etc/php.ini
+    if [ -z "`grep -E ^${NAME} /etc/php.ini`" ] ; then 
+	echo "${NAME} = ${VALUE}" >> /etc/php.ini
+	echo "Added"
+    fi
+}
+
+
 touch /var/log/php-fpm/www-error.log
 
 
@@ -7,6 +20,15 @@ if [ ! -z "${PHP_FPM_USER}" ] ; then
     sed -i "/^user =/ s/.*/user = ${PHP_FPM_USER}/" /etc/php-fpm.d/www.conf
     chown ${PHP_FPM_USER} /var/log/php-fpm/www-error.log 
     chown -R ${PHP_FPM_USER} /var/lib/php/session
+fi
+
+ARRAY_PHP=`set | grep -E ^PHPINI_`
+if [ ! -z "${ARRAY_PHP}" ] ; then
+    ARRAY_PHP=`echo ${ARRAY_PHP} | sed s/PHPINI_//g | sed s/__/./g`
+    for PARAM in ${ARRAY_PHP} ; do
+	modify_php_ini ${PARAM}
+    done
+    
 fi
 
 shutdown_php_fpm()
@@ -23,3 +45,4 @@ trap shutdown_php_fpm SIGINT SIGTERM SIGHUP
 tail -f /var/log/php-fpm/www-error.log &
 
 wait
+
